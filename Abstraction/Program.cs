48d8d30bc737d708a7f90c837c54bda3d7950f05
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-13 16:15:53 +0530

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Abstract
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea(); 
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Square : Shape
+    {
+        public double side { get; set; }
+        public override double GetArea()
+        {
+            return side * side;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myShape = new Circle { Radius = 3.74 };
+            Console.WriteLine(myShape.GetArea());
+
+            Shape myShape2 = new Square { side = 4 };
+            Console.WriteLine(myShape2.GetArea());
+        }
+    }
+}
